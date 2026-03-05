2551160e0c31f15c7cb6e403161714c4799eef29
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2026-03-05 13:09:02 +0800

    update impl to reflect github actions status key-value changes; switch yarn to pnpm; upgrade jest & got

---
+++ set-env.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+export PS1="(devbox)$PS1"
+export NODE_PATH="$(readlink -e $(which node) | sed -E 's/\/bin\/node//g')"
+export SOURCE_REPO_NAME=${PWD##*/}
+
+echo "###################################################################################################################"
+echo "                                                                                  "
+echo "##   !! $SOURCE_REPO_NAME DEVELOPMENT ENVIRONMENT ;) !!"
+echo "##   DevBox VERSION: `devbox version`                 "
+echo "##   NODE_PATH: $NODE_PATH                            "
+echo "##   NODE VERSION: `node --version`                   "
+echo "                                                                                  "
+echo "###################################################################################################################"
+
+echo "Setting up PNPM..."
+corepack prepare pnpm@10.30.3 --activate
+corepack use pnpm@10.30.3
+pnpm --frozen-lockfile recursive install
